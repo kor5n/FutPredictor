5962c40269f0062e09e3ce44f01ae6db6b3da6c7
Kor5n <kor5n@Kor5ns-MacBook-Pro.local>
2025-05-30 13:21:45 +0200

    Added loading component to the slow player list rendering

--- frontend/fut-predictor/src/components/PlayerList.tsx
+++ frontend/fut-predictor/src/components/PlayerList.tsx
@@ -1,18 +1,29 @@
 import '../App.css'
 import "../assets/playerlist.css"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 interface PlayerData {
     stats: string[]
 }
 
 export default function PlayerList({stats} : PlayerData){
+
+    const [showFirst, setShowFirst] = useState(true)
+
+    useEffect(() => {
+        if(stats.length > 0) {
+            requestAnimationFrame(() =>{
+                setShowFirst(false)
+            })
+        }
+    }, [stats])
     /*<p className='player-text' key={index}>
                     {item}
                     </p>*/
     return(
         <div className='player-list'>
-            <table className='player-table'>
+            {showFirst && (<p>Loading...</p>)}
+            {!showFirst && (<table className='player-table'>
                 {stats.map((item, index)=> (
                     
                         
@@ -30,7 +41,7 @@ export default function PlayerList({stats} : PlayerData){
                     
                     
                 ))}
-            </table>
+            </table>)}
         </div>
     )
 }
