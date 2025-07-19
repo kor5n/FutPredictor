dcd8d6f680c9e349457936b60691f0c9b02a4683
kor5n <jegor.kotov@gmail.com>
2025-07-19 16:21:46 +0200

    improved css and little optimizations

--- frontend/fut-predictor/src/components/PlayerList.tsx
+++ frontend/fut-predictor/src/components/PlayerList.tsx
@@ -17,29 +17,17 @@ export default function PlayerList({stats} : PlayerData){
             })
         }
     }, [stats])
-    /*<p className='player-text' key={index}>
-                    {item}
-                    </p>*/
+
     return(
         <div className='player-list'>
             {showFirst && (<p>Loading...</p>)}
             {!showFirst && (<table className='player-table'>
                 {stats.map((item, index)=> (
-                    
-                        
                     <tr key={index}>
-                        <th><p className='player-text'>{item.split("|")[0]}</p></th>
-                        <th><p className='player-text'>{item.split("|")[1]}</p></th>
-                        <th><p className='player-text'>{item.split("|")[2]}</p></th>
-                        <th><p className='player-text'>{item.split("|")[3]}</p></th>
-                        <th><p className='player-text'>{item.split("|")[4]}</p></th>
-                        <th><p className='player-text'>{item.split("|")[5]}</p></th>
-                        <th><p className='player-text'>{item.split("|")[6]}</p></th>
-                        <th><p className='player-text'>{item.split("|")[7]}</p></th>
-                        <th><p className='player-text'>{item.split("|")[8]}</p></th>
+                        {item.split("|").map((el) => (
+                            <th><p className='player-text'>{el}</p></th>
+                        ))}
                     </tr>
-                    
-                    
                 ))}
             </table>)}
         </div>
