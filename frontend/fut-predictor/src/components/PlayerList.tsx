f7b274833603d868c8795f68d24f89ecd520dee4
kor5n <jegor.kotov@gmail.com>
2025-08-07 20:38:40 +0200

    optimized playerlist by splitting it up in pages with 100 players each

--- frontend/fut-predictor/src/components/PlayerList.tsx
+++ frontend/fut-predictor/src/components/PlayerList.tsx
@@ -4,32 +4,70 @@ import { useEffect, useState } from 'react'
 
 interface Props {
     stats: string[],
-    search_query?: string,
+    page: number,
 }
 
-export default function PlayerList({stats} : Props){
+interface ListItemProps{
+    item: string,
+    index: number,
+}
 
-    const [showFirst, setShowFirst] = useState(true)
+function ListItem({item, index}:ListItemProps){
+    return(
+        <tr key={index}>
+            {item.split("|").map((el, index) => (
+                <th key={index}><p className='player-text'>{el}</p></th>
+            ))}
+        </tr>
+    )
+}
+
+export default function PlayerList({stats, page} : Props){
+
+    
+
+    const [showFirst, setShowFirst] = useState(true);
+    const [splitStats, setSplit] = useState<string[][]>();
 
     useEffect(() => {
         if(stats.length > 0) {
+        
+            let tmp_array: string[][] = [];
+            let tmp_element: string[] = [];
+
+            stats.forEach((element, index) => {
+                tmp_element.push(element);
+
+                if ((index + 1) %100 === 0) {
+                    tmp_array.push(tmp_element);
+                    tmp_element = [];
+                }
+            });
+
+            if (tmp_element.length > 0) {
+                tmp_array.push(tmp_element);
+            }
+
+            setSplit(tmp_array);
             requestAnimationFrame(() =>{
                 setShowFirst(false)
             })
         }
+        
     }, [stats])
 
     return(
         <div className='player-list'>
             {showFirst && (<p>Loading...</p>)}
-            {!showFirst && (<table className='player-table'>
-                {stats.map((item, index)=> (
-                    <tr key={index}>
-                        {item.split("|").map((el) => (
-                            <th><p className='player-text'>{el}</p></th>
-                        ))}
-                    </tr>
-                ))}
+            {!showFirst && (
+            <table className='player-table'>
+                {splitStats && splitStats[0] ? (
+                    splitStats[page -1].map((item, index) => (
+                        <ListItem item={item} index={index} key={index} />
+                    ))
+                    ) : (
+                    <p>Loading or no data</p>
+                )}
             </table>)}
         </div>
     )
--- frontend/fut-predictor/src/pages/Ratings.tsx
+++ frontend/fut-predictor/src/pages/Ratings.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react"
 import PlayerList from "../components/PlayerList"
+import "../assets/ratings.css"
 
 export default function(){
     const [error, setError] = useState<boolean>(false);
     const [stats, setStats] = useState<string[]>([]);
     const [searchPrompt, setPrompt] = useState<string>("null");
+    const [pageNum, setPage] = useState<number>(1);
 
     const ChangePrompt = (event: any) => {
       const { value } = event.target;
@@ -15,6 +17,7 @@ export default function(){
 
     const SubmitPrompt = async (event : any) =>{
       event.preventDefault();
+      setPage(1);
       await fetchData();
     }
 
@@ -50,6 +53,15 @@ export default function(){
           }
     };
 
+    const ChangePage = (increase:number) =>{
+      let new_page:number = pageNum + increase;
+      if (new_page <=0){
+        setPage(pageNum);
+      } else{
+        setPage(new_page);
+      }
+    }
+
     useEffect(() =>{ 
       fetchData()
     }, [])
@@ -63,7 +75,14 @@ export default function(){
           {error ? (
               <p>Error loading player names.</p>
           ) : (
-              <PlayerList stats={stats} />
+              <>
+                <PlayerList stats={stats} page={pageNum} />
+                <div className="page-btn-div">
+                  {pageNum >= 2 ? <button className="page-btn prev-page-btn" onClick={() =>{ChangePage(-1)}}>previous page</button> : null}
+                  {pageNum<Math.round(stats.length/100)&&stats.length >= 51 ? <button className="page-btn next-page-btn" onClick={() =>{ChangePage(1)}}>next page</button>: null}
+                </div>
+                {stats.length >= 101 ? <p className="cur-page">page: {pageNum}/{Math.round(stats.length/100)}</p>: null}
+              </>
           )}
         </>
     );
